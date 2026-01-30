19de27041449426a3f03efee101f3acb681f45a7
ikun <144520346+maleday@users.noreply.github.com>
2026-01-30 23:12:14 +0800

    Refactor proxy.ts to use single quotes and clean up code

--- proxy.ts
+++ proxy.ts
@@ -1,34 +1,32 @@
-// src/proxy.ts 或 proxy.ts（按你项目实际位置）
-import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+import { NextRequest, NextResponse } from 'next/server'
+import { getSessionCookie } from 'better-auth/cookies'
 
 export async function proxy(request: NextRequest) {
   const sessionCookie = getSessionCookie(request, {
-    cookiePrefix: "pic-impact",
-  });
-
-  if (request.nextUrl.pathname.startsWith("/api/v1") && !sessionCookie) {
+    cookiePrefix: 'pic-impact'
+  })
+  if (request.nextUrl.pathname.startsWith('/api/v1') && !sessionCookie) {
     return Response.json(
-      { success: false, message: "authentication failed" },
+      { success: false, message: 'authentication failed' },
       { status: 401 }
-    );
+    )
+  }
+  if (request.nextUrl.pathname.startsWith('/admin') && !sessionCookie) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  if (sessionCookie && request.nextUrl.pathname === '/login') {
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
-  if (request.nextUrl.pathname.startsWith("/admin") && !sessionCookie) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  if (sessionCookie && request.nextUrl.pathname === "/login") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
+// Optionally, don't invoke Middleware on some paths
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-    "/admin/:path*",
-    "/api/v1/:path*",
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/admin/:path*',
+    '/api/v1/:path*',
   ],
-};
+}
